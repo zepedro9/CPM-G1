80d03b5e8752ad193f7b4b3638f4ab206a8a41a6
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-05-08 08:38:26 +0100

    feat(server/products): added request to get list of products.
    An example of new request is: http://localhost:3000/api/products/list?prod=48413778121&prod=61234567890&prod=26761059180

--- Assignment_#1/server/api/routes/products.js
+++ Assignment_#1/server/api/routes/products.js
@@ -7,9 +7,22 @@ router.get("/", async(req, res) => {
   res.send(products); 
 });
 
+router.get("/list", async(req, res) => {
+  let products = req.query.prod;
+  let response = []
+  for (let id of products) {
+    const product = await Product.findOne({id: id}); 
+    response.push(product);
+  }
+  console.log(response);
+  res.status(200).send({message: "OK", products: response}); 
+});
+
 router.get("/:id", async(req, res) => {
     const products = await Product.find({id: req.params.id});
     res.send(products);
 });
+
+
 
 module.exports = router; 
