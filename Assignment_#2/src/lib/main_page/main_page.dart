32331c10592dc3dd49f406cd40daceda8a2be61d
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-05-20 14:57:31 +0100

    feat(main_page): main page boilerplate
    Separate the main page in a different file and added a color to the app.

---
+++ Assignment_#2/src/lib/main_page/main_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("It's raining man, aleluja",
+                style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      ),
+    );
+  }
+}
