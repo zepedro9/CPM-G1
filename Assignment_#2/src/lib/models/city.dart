1072bb2617bf43f8494d06f064eff8d01162edd4
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-05-22 17:26:24 +0100

    feat(databases): starting server
    Co-authored-by: Diana Freitas <dianaamfr@users.noreply.github.com>

---
+++ Assignment_#2/src/lib/models/city.dart
@@ -0,0 +1,26 @@
+class City {
+  final int id;
+  final String name;
+  final bool isOfInterest;
+
+  const City({
+    required this.id,
+    required this.name,
+    required this.isOfInterest
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'isOfInterest': isOfInterest,
+    };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'City{id: $id, name: $name, isOfInterest: $isOfInterest}';
+  }
+}
