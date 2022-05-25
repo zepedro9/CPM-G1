9b9a3646c700dac148a440b0bddb19bc3f2c32c5
Diana Freitas <up201806230@fe.up.pt>
2022-05-25 13:41:59 +0100

    Refactor: deleted unused widgets and created citiesListView widget

--- Assignment_#2/src/lib/manage_locations_page/manage_locations.dart
+++ Assignment_#2/src/lib/manage_locations_page/manage_locations.dart
@@ -14,7 +14,7 @@ class ManageLocationsPage extends StatefulWidget {
 class _ManageLocationsPageState extends State<ManageLocationsPage> {
   List<City> cities = [];
 
-  Future fetchCities() async { 
+  Future fetchCities() async {
     List<City> response = await DBHelper.instance.getCities();
     setState(() {
       cities = response;
@@ -29,40 +29,16 @@ class _ManageLocationsPageState extends State<ManageLocationsPage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 247, 247, 247),
         appBar: AppBar(
           title: const Text('Manage Locations'),
           backgroundColor: const Color.fromRGBO(32, 82, 209, 1),
         ),
-        body: ListView.builder(
-          itemCount: cities.length,
-          itemBuilder: (BuildContext context, int index) =>
-            CityCheckBoxTile(
-              title: cities[index].name,
-              value: cities[index].isOfInterest,
-              onChanged: (bool newValue) {
-                print(cities[index].isOfInterest);
-                setState(() {
-                  cities[index].isOfInterest = newValue;
-                });
-                print(cities[index].isOfInterest);
-              },
-            )
-        )
-    );
+        body: citiesListView());
   }
 
-  FutureBuilder<List<City>> citiesFutureBuilder() {
-    Future<List<City>> citiesFuture = DBHelper.instance.getCities();
-    Widget body(data) => citiesListView(data);
-
-    return getCityListFutureBuilder(citiesFuture, body);
-  }
-
-  Widget citiesListView(List<City> data) {
-    cities = data;
+  Widget citiesListView() {
     return ListView.builder(
         itemCount: cities.length,
         itemBuilder: (BuildContext context, int index) => CityCheckBoxTile(
--- Assignment_#2/src/lib/utils/future_builder.dart
+++ Assignment_#2/src/lib/utils/future_builder.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
-import '../databases/database.dart';
-import '../models/city.dart';
-
 /// Renders a response for an http request.
 FutureBuilder<String> getStringFutureBuilder(
     Future<String> response, Widget Function(dynamic) widget) {
@@ -12,22 +9,6 @@ FutureBuilder<String> getStringFutureBuilder(
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return widget(json.decode(snapshot.data!));
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        } else {
-          return const CircularProgressIndicator();
-        }
-      });
-}
-
-// Returns a FutureBuilder for a list of cities.
-FutureBuilder<List<City>> getCityListFutureBuilder(
-  Future<List<City>> futureCities, Widget Function(dynamic) listViewWidget) {
-  return FutureBuilder<List<City>>(
-      future: futureCities,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return listViewWidget(snapshot.data!);
         } else if (snapshot.hasError) {
           return Text('${snapshot.error}');
         } else {
